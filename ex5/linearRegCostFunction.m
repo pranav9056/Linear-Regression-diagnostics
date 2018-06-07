92f65cdaca10c31c41cbcd1a2d5b625d3917ef0b
Pranav <pranavjain3194@gmail.com>
2018-06-07 16:35:20 -0400

    added linearRegression cost

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -9,15 +9,14 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+yPredicted = X * theta;
+temp = yPredicted - y;
+temp = temp .^ 2;
+J = sum(temp)/(2*m);
+J = J + lambda/(2*m) * sum(theta(2:end) .^ 2); 
+
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
 
 
 
